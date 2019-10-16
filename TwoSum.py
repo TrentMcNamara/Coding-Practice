649626c5a2283122017daab887176829a345a771
Trent McNamara <trent_mcnamara@mac.com>
2019-10-16 12:24:19 -0500

    Solution Attempt #2

--- TwoSum.py
+++ TwoSum.py
@@ -3,28 +3,33 @@
 
 # Brute Force Solution 
 class Solution:
-    def twoSum(nums, target):
-        
+    def twoSum_brute(nums, target):
         solved = 0
-        
         index_i = 0
         for i in nums:
-            
             index_j = 0
             for j in nums:
-                
                 if ( i !=j ):
                     summ = i + j
-                    
                     if summ == target:
                         return [index_i,index_j]
                         solved = 1
-                    
                 index_j += 1
-            
             index_i +=1
-        
         if (solved != 1):
             return 'no solution' 
     
-print(Solution.twoSum([1,2,3,6,10], 12))
+print(Solution.twoSum_brute([1,2,3,6,10], 3))
+
+# Attempt 2
+class Solution:
+    def twoSum_two(nums, target):
+        solved = 0
+        for i in nums:
+            if target - i in nums and (i != target-i):
+                return [nums.index(i), nums.index(target-i)]
+                solved = 1
+        if (solved != 1):
+            return 'no solution'
+    
+print(Solution.twoSum_two([1,2,3,6,10], 3))
