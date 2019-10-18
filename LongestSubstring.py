f8c43ccb3ed4cacd69223867882bb12a10bf726d
Trent McNamara <trent_mcnamara@mac.com>
2019-10-18 11:16:46 -0500

    New attempt

--- LongestSubstring.py
+++ LongestSubstring.py
@@ -1,13 +1,15 @@
 # Given a string, find the length of the longest substring without repeating characters.
 
 class Solution:
-    def lengthOfLongestSubstring(s):
-        count = 0
-        for i in set(s):
-            if i in s:
-                count +=1
-        
-        return set(s)
-
+    def lengthOfLongestSubstring(word):
+        n = len(word)
+        longest = 0
+        for i in range(n):
+            seen = set()
+            for j in range(i, n):
+                if word[j] in seen: break
+                seen.add(word[j])
+            longest = max(len(seen), longest)
+        return longest
  
 print(Solution.lengthOfLongestSubstring('abcabcabd'))
